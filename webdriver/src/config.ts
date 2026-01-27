e1f1ab759d3860fd6a8ed0e54b2f906a6ed4165c
Taylor Vann <brian.t.vann@gmail.com>
2026-01-27 15:31:31 -0800

    from string

--- webdriver/src/config.ts
+++ webdriver/src/config.ts
@@ -1,1 +1,41 @@
+export interface ConfigParams {
+	host_and_port: string;
+	webdrivers: [string, string][];
+}
+
+export interface ConfigJson {
+	host_and_port: URL;
+	webdrivers: [string, URL][];
+}
+
+function fromString(jsonStr: string): ConfigJson | Error {
+	let json: any;
+	try {
+		json = JSON.parse(jsonStr);
+	} catch (e) {
+		if (e instanceof Error) return e;
+		return new Error("failed to parse config from string");
+	}
+
+	let host_and_port: URL | null = URL.parse(json.host_and_port);
+	if (!host_and_port) return new Error(`JSON: invalid host_and_port`);
+
+	let webdrivers: [string, URL][] = [];
+	if (Array.isArray(json.webdrivers))
+		for (let [command, targetUrl] of json.webdrivers) {
+			if (typeof command === "string") {
+				let url = URL.parse(targetUrl);
+				if (!url) return new Error(`JSON: invalid webdriver url`);
+				webdrivers.push([command, url]);
+			} else {
+				return new Error(`JSON: invalid webdriver commmand`);
+			}
+		}
+
+	return {
+		host_and_port,
+		webdrivers,
+	};
+}
+
 export {};
