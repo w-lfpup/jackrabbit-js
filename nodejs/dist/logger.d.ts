41cc83da80a77be91198c000577b48b3a8fbd047
Taylor Vann <brian.t.vann@gmail.com>
2026-02-09 16:08:31 -0800

    builds added

--- nodejs/dist/logger.d.ts
+++
@@ -1,7 +0,0 @@
-import type { LoggerAction, LoggerInterface, TestModule } from "../../core/dist/mod.js";
-export declare class Logger implements LoggerInterface {
-    #private;
-    get failed(): boolean;
-    get cancelled(): boolean;
-    log(testModules: TestModule[], action: LoggerAction): void;
-}
--- nodejs/dist/runner.d.ts
+++
@@ -1,6 +0,0 @@
-import { Logger } from "./logger.js";
-export declare class Config {
-    files: string[];
-    constructor(args: string[]);
-}
-export declare function run(config: Config, logger?: Logger): Promise<void>;
