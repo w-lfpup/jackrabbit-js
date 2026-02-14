5c826208c5f996fca60939f014ef2bb09df1f1b7
Taylor Vann <brian.t.vann@gmail.com>
2026-02-13 16:25:36 -0800

    add error catches per module run

--- browser/src/mod.ts
+++ browser/src/mod.ts
@@ -1,17 +1,9 @@
 import { Logger } from "./logger.js";
 import { run } from "./runner.js";
 
-const logger = new Logger();
+let jackrabbitMap = document.querySelector("script[type=jackrabbitmap]");
+if (null === jackrabbitMap) throw new Error("jackrabbitmap not found");
+let jackrabbitConfig = JSON.parse(jackrabbitMap.textContent);
 
-try {
-	let jackrabbitMap = document.querySelector("script[type=jackrabbitmap]");
-	if (null === jackrabbitMap) throw new Error("jackrabbitmap not found");
-	let jackrabbitConfig = JSON.parse(jackrabbitMap.textContent);
-
-	await run(jackrabbitConfig.test_collections, logger);
-} catch (e: unknown) {
-	logger.log({
-		type: "error",
-		error: e?.toString() ?? "wild horses error",
-	});
-}
+let logger = new Logger();
+run(jackrabbitConfig.test_collections, logger);
--- browser/src/runner.ts
+++ browser/src/runner.ts
@@ -1,13 +1,20 @@
 import { startRun } from "jackrabbit/core/dist/mod.js";
 
-import { Logger } from "./logger.js";
+import type { Logger } from "./logger.js";
 
-export async function run(files: string[], logger: Logger = new Logger()) {
+export async function run(files: string[], logger: Logger) {
 	for (const url of files) {
-		let filepath = URL.parse(import.meta.url, url);
-		if (filepath) {
+		try {
+			let filepath = URL.parse(import.meta.url, url);
+			if (null === filepath) throw new Error("Failed to import url: " + url);
+
 			const { testModules } = await import(filepath.toString());
 			await startRun(logger, testModules);
+		} catch (e: unknown) {
+			logger.log({
+				type: "error",
+				error: e?.toString() ?? "wild horses error",
+			});
 		}
 	}
 }
