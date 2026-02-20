3eb6136e79a32e5bcf281e88438a133fc1a46f9c
Taylor Vann <brian.t.vann@gmail.com>
2026-02-19 20:07:09 -0800

    format

--- core/src/run_steps.ts
+++ core/src/run_steps.ts
@@ -22,83 +22,87 @@ async function createTimeout(
 	return `timed out at ${performance.now()} after ${timeoutMs} ms.`;
 }
 
-async function execTest(
+async function prepareTest(
 	testModules: TestModule[],
 	logger: LoggerInterface,
+	collection_id: number,
 	module_id: number,
 	test_id: number,
 ) {
 	const { tests, options } = testModules[module_id];
-	const module_name = options?.title ?? module_id.toString();
-	const test_Name =
+	const test_name =
 		testModules[module_id]?.tests[test_id]?.name ?? test_id.toString();
 
 	// should be a promise
-
-	logger.log({
-		type: "start_test",
-		module_id,
-		test_id,
-		test_name,
-	});
-
-	const testFunc = tests[test_id];
-	const startTime = performance.now();
-	let assertions: Assertions;
-	try {
-		assertions = await Promise.race([
-			createTimeout(options?.timeoutMs),
-			testFunc(),
-		]);
-	} catch (e: unknown) {
-		return logger.log({
-			type: "test_error",
+	return async function () {
+		logger.log({
+			collection_id,
 			module_id,
-			module_name,
 			test_id,
 			test_name,
-			error: e?.toString() ?? "wild test error",
+			type: "start_test",
 		});
-	}
 
-	const end_time = performance.now();
+		const testFunc = tests[test_id];
+		const start_time = performance.now();
+		let assertions: Assertions;
+		try {
+			assertions = await Promise.race([
+				createTimeout(options?.timeoutMs),
+				testFunc(),
+			]);
+		} catch (e: unknown) {
+			return logger.log({
+				collection_id,
+				error: e?.toString() ?? "wild test error",
+				module_id,
+				test_id,
+				type: "test_error",
+			});
+		}
 
-	logger.log({
-		type: "end_test",
-		assertions,
-		end_time,
-		module_id,
-		module_name,
-		startTime,
-		test_id,
-		test_name,
-	});
+		const end_time = performance.now();
+
+		logger.log({
+			assertions,
+			collection_id,
+			end_time,
+			module_id,
+			start_time,
+			test_id,
+			type: "end_test",
+		});
+	};
 }
 
 async function execCollection(
-	testModules: TestModule[],
 	logger: LoggerInterface,
+	testModules: TestModule[],
+	collection_id: number,
 	module_id: number,
 ) {
 	const { tests } = testModules[module_id];
 
 	const wrappedTests = [];
 	for (let [test_id] of tests.entries()) {
-		wrappedTests.push(execTest(testModules, logger, module_id, test_id));
+		wrappedTests.push(
+			prepareTest(testModules, logger, collection_id, module_id, test_id),
+		);
 	}
 
 	await Promise.all(wrappedTests);
 }
 
 async function execCollectionOrdered(
-	testModules: TestModule[],
 	logger: LoggerInterface,
+	testModules: TestModule[],
+	collection_id: number,
 	module_id: number,
 ) {
 	const { tests } = testModules[module_id];
 
 	for (let [test_id] of tests.entries()) {
-		await execTest(testModules, logger, module_id, test_id);
+		await prepareTest(testModules, logger, collection_id, module_id, test_id);
 	}
 }
 
@@ -129,8 +133,13 @@ export async function runCollection(
 		});
 
 		options?.runAsynchronously
-			? await execCollection(testModules, logger, module_id)
-			: await execCollectionOrdered(testModules, logger, module_id);
+			? await execCollection(logger, testModules, collection_id, module_id)
+			: await execCollectionOrdered(
+					logger,
+					testModules,
+					collection_id,
+					module_id,
+				);
 
 		logger.log({
 			collection_id,
