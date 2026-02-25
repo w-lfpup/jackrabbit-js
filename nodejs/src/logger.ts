7629024328167ef1cc85d5f0b69f98614ef88f8c
Taylor Vann <brian.t.vann@gmail.com>
2026-02-25 15:37:37 -0800

    build results

--- nodejs/src/logger.ts
+++ nodejs/src/logger.ts
@@ -17,6 +17,23 @@ interface Receipts {
 	errors: LoggerAction[];
 }
 
+interface TestResults {
+	loggerStartAction: LoggerAction;
+	loggerEndAction: LoggerAction | undefined;
+}
+
+interface ModuleResults {
+	loggerAction: LoggerAction;
+	tests: Map<number, TestResults>;
+}
+
+interface CollectionResults {
+	loggerAction: LoggerAction;
+	modules: Map<number, ModuleResults>;
+}
+
+type Results = Map<number, CollectionResults>;
+
 export class Logger implements LoggerInterface {
 	#receipts: Receipts = {
 		runs: [],
@@ -53,7 +70,7 @@ export class Logger implements LoggerInterface {
 			this.#data.endTime = action.time;
 
 			// buildResults
-			logResults(this.#data, this.#receipts);
+			logRun(this.#data, this.#receipts);
 		}
 
 		if ("start_collection" === action.type) {
@@ -100,7 +117,7 @@ export class Logger implements LoggerInterface {
 	}
 }
 
-function logResults(data: LoggerData, receipts: Receipts) {
+function logRun(data: LoggerData, receipts: Receipts) {
 	let status_with_color = data.failed
 		? yellow("\u{2717} failed")
 		: blue("\u{2714} passed");
@@ -109,11 +126,14 @@ function logResults(data: LoggerData, receipts: Receipts) {
 		status_with_color = gray("\u{2717} errored");
 	}
 
-	const overhead = data.endTime - data.startTime;
+	let results = buildResults(receipts);
+	logResults(results);
+
+	const total = data.endTime - data.startTime;
 	console.log(`Results:
 ${status_with_color}
   duration: ${data.testTime.toFixed(4)} mS
-  overhead: ${overhead.toFixed(4)} mS`);
+  total: ${total.toFixed(4)} mS`);
 }
 
 // 39 - default foreground color
@@ -129,3 +149,61 @@ function yellow(text: string) {
 function gray(text: string) {
 	return `\x1b[100m\x1b[97m${text}\x1b[0m`;
 }
+
+function buildResults(receipts: Receipts): Results {
+	let results = new Map<number, CollectionResults>();
+
+	for (let loggerAction of receipts.collections) {
+		if ("start_collection" === loggerAction.type) {
+			results.set(loggerAction.collection_id, {
+				loggerAction,
+				modules: new Map(),
+			});
+		}
+	}
+
+	for (let loggerAction of receipts.modules) {
+		if ("start_module" === loggerAction.type) {
+			let collection = results.get(loggerAction.collection_id);
+			if (collection)
+				collection.modules.set(loggerAction.module_id, {
+					loggerAction,
+					tests: new Map(),
+				});
+		}
+	}
+
+	for (let loggerAction of receipts.tests) {
+		if ("start_test" === loggerAction.type) {
+			let collection = results.get(loggerAction.collection_id);
+			if (collection) {
+				let module = collection.modules.get(loggerAction.collection_id);
+				if (module)
+					module.tests.set(loggerAction.test_id, {
+						loggerStartAction: loggerAction,
+						loggerEndAction: undefined,
+					});
+			}
+		}
+	}
+
+	for (let loggerAction of receipts.testResults) {
+		if (
+			"end_test" === loggerAction.type ||
+			"test_error" === loggerAction.type
+		) {
+			let collection = results.get(loggerAction.collection_id);
+			if (collection) {
+				let module = collection.modules.get(loggerAction.collection_id);
+				if (module) {
+					let testResult = module.tests.get(loggerAction.test_id);
+					if (testResult) testResult.loggerEndAction = loggerAction;
+				}
+			}
+		}
+	}
+
+	return results;
+}
+
+function logResults(results: Results) {}
