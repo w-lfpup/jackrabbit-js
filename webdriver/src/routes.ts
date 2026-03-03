9c451efe627de76ed4e1f12e43fcef89d4fe9992
Taylor Vann <brian.t.vann@gmail.com>
2026-03-03 14:15:34 -0800

    promisify responses

--- webdriver/src/routes.ts
+++ webdriver/src/routes.ts
@@ -43,59 +43,16 @@ export class Router {
 
 	#boundRoute = this.#route.bind(this);
 	async #route(req: IncomingMessage, res: ServerResponse) {
-		let { url, method } = req;
-
 		if (serveBadRequest(req, res)) return;
 		if (servePing(req, res)) return;
 		if (serveTestPage(req, res, this.#config)) return;
-
-		// "test" home page
-		if (url === "/" && "GET" === method) {
-			let hangar = testHanger({
-				jackrabbit_url: this.#config.hostAndPort,
-				test_collections: process.argv.slice(3),
-			});
-
-			res.setHeader("Content-Type", "text/html");
-			res.writeHead(200);
-			return res.end(hangar);
-		}
-
-		// log test actions
-		if (url?.startsWith("/log/") && "POST" === method) {
-			let id: string | undefined;
-			let cookies = req.headers.cookie?.split(";") ?? [];
-			for (const cookieLine of cookies) {
-				if (cookieLine.startsWith("jackrabbit=")) {
-					let [_name, value] = cookieLine.split("=");
-					id = value;
-				}
-			}
-
-			if (id) {
-				let loggerAction = await getLoggerActionFromRequestBody(req);
-				this.#eventbus.dispatchAction({
-					type: "log",
-					// urlStr: req.url,
-					loggerAction,
-					id,
-				});
-				res.writeHead(200);
-			} else {
-				res.writeHead(403);
-			}
-
-			return res.end();
-		}
+		if (logAction(req, res, this.#eventbus)) return;
 
 		await serveFile(req, res);
 	}
 }
 
-function serveBadRequest(
-	req: IncomingMessage,
-	res: ServerResponse,
-): boolean {
+function serveBadRequest(req: IncomingMessage, res: ServerResponse): boolean {
 	let { url } = req;
 	if (url) return false;
 
@@ -106,10 +63,7 @@ function serveBadRequest(
 	return true;
 }
 
-function servePing(
-	req: IncomingMessage,
-	res: ServerResponse,
-): boolean {
+function servePing(req: IncomingMessage, res: ServerResponse): boolean {
 	let { url, method } = req;
 	if (url !== "/ping" || "GET" !== method) return false;
 
@@ -126,7 +80,6 @@ function serveTestPage(
 	config: ConfigInterface,
 ): boolean {
 	let { url, method } = req;
-
 	if (url !== "/" || "GET" !== method) return false;
 
 	let hangar = testHanger({
@@ -137,7 +90,48 @@ function serveTestPage(
 	res.setHeader("Content-Type", "text/html");
 	res.writeHead(200);
 	res.end(hangar);
-	
+
+	return true;
+}
+
+function logAction(
+	req: IncomingMessage,
+	res: ServerResponse,
+	eventbus: EventBus,
+): boolean {
+	let { url, method } = req;
+	if (!url?.startsWith("/log/") || "POST" !== method) return false;
+
+	let id: string | undefined;
+	let cookies = req.headers.cookie?.split(";") ?? [];
+	for (const cookieLine of cookies) {
+		if (cookieLine.startsWith("jackrabbit=")) {
+			let [_name, value] = cookieLine.split("=");
+			id = value;
+		}
+	}
+
+	if (id) {
+		getLoggerActionFromRequestBody(req)
+			.then(function (loggerAction: LoggerAction) {
+				eventbus.dispatchAction({
+					type: "log",
+					loggerAction,
+					id,
+				});
+				res.writeHead(201);
+			})
+			.catch(function () {
+				res.writeHead(401);
+			})
+			.finally(function () {
+				res.end();
+			});
+	} else {
+		res.writeHead(401);
+		res.end();
+	}
+
 	return true;
 }
 
