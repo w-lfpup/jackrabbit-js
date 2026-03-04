f2d23cfb0d3b019269924a4cc42656cd919054c7
Taylor Vann <brian.t.vann@gmail.com>
2026-03-04 15:28:04 -0800

    add config to readme

--- webdriver/src/config.ts
+++ webdriver/src/config.ts
@@ -25,6 +25,7 @@ export async function createConfig(
 	let relPath = path.join(process.cwd(), configFilepath);
 
 	try {
+		// windows might need a "file://<relPath>" situation
 		let { default: json } = await import(relPath, { with: { type: "json" } });
 
 		let hostAndPort: URL | null = URL.parse(json.host_and_port);
